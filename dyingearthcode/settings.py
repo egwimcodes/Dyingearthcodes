727b177330780d2f3fc6bc230efa50e5ad87047b
egwimcodes <egwimcodes@gmail.com>
2024-03-10 17:09:56 +0100

    reconfigured postgres and added gunicorn

--- dyingearthcode/settings.py
+++ dyingearthcode/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 from jazzmin import *
 from dotenv import load_dotenv
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 SITE_ID = 1
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 # Application definition
 INSTALLED_APPS = [
 
@@ -117,6 +117,10 @@ DATABASES = {
         'PORT': os.environ.get('PORT')
     }
 }
+
+
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

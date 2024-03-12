eb0934b19f6220f242efc686f8ec21a10ff36b6a
egwimcodes <egwimcodes@gmail.com>
2024-03-12 16:45:26 +0100

    fixing static

--- dyingearthcode/urls.py
+++ dyingearthcode/urls.py
@@ -44,5 +44,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

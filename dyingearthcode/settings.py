80e9b8bfab661217d335d8275b147d3f3bbf05c6
egwimcodes <egwimcodes@gmail.com>
2024-03-12 16:19:39 +0100

    worked on responsiveness and error handlig

--- dyingearthcode/settings.py
+++ dyingearthcode/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 SITE_ID = 1
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')

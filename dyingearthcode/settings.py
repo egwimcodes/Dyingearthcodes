9498cbfdfb41c57b8fef792ca8326f021d518ff7
egwimcodes <egwimcodes@gmail.com>
2024-03-12 10:49:05 +0100

    fixed the static error uvicorn

--- dyingearthcode/settings.py
+++ dyingearthcode/settings.py
@@ -216,11 +216,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', )
 
 MEDIA_ROOT = BASE_DIR / "static/images/"
 
